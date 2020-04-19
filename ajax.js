f1c0fdc4f190084dc3dbefd00f8d9a06950f6e2d
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2020-04-19 22:41:59 +0200

    Quitando cabeceras

--- ajax.js
+++ ajax.js
@@ -1,12 +1,8 @@
 function loadLDocA(fichero,tipo)
 {
     let http = new XMLHttpRequest(); //Se crea petición al servidor
-    if ("withCredentials" in http) {
-        alert("Cors habilitado")
-    }
     http.open("GET",fichero,true); //Se pide procesar el fichero. Continua la ejecución hasta recibir la respuesta
     http.setRequestHeader("Content-type", "text/xml");
-   // http.setRequestHeader('Access-Control-Allow-Origin', '*');
     http.send();
     http.addEventListener('load', (event) => {  //Cuando se reciba la respuesta, se ejecuta esta función
         if(http.status === 200) {
